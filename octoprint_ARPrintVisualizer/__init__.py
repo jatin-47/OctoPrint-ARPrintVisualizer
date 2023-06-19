a589df7edef16a983f0d71d6c0b3103cbeec6534
Jatin Saini <67009939+jatin-47@users.noreply.github.com>
2023-06-19 13:32:59 -0400

    Added webcam support

--- octoprint_ARPrintVisualizer/__init__.py
+++ octoprint_ARPrintVisualizer/__init__.py
@@ -1,34 +1,115 @@
 # coding=utf-8
 from __future__ import absolute_import
+import threading
+import requests
 import octoprint.plugin
+from octoprint.schema.webcam import Webcam, WebcamCompatibility
+from octoprint.webcams import WebcamNotAbleToTakeSnapshotException
 
 class ARPrintVisualizerPlugin(octoprint.plugin.SettingsPlugin,
                               octoprint.plugin.TemplatePlugin,
-                              octoprint.plugin.AssetPlugin):
-
-    ##~~ SettingsPlugin mixin
-    def get_settings_defaults(self):
-        return {
-            # put your plugin's default settings here
-        }
-
-    ##~~ TemplatePlugin mixin
-    def get_template_configs(self):
-        return [
-            dict(type="navbar", custom_bindings=False),
-            dict(type="settings", custom_bindings=False)
-        ]
+                              octoprint.plugin.AssetPlugin,
+                              octoprint.plugin.WebcamProviderPlugin):
+    
+    def __init__(self):
+        self._capture_mutex = threading.Lock()
 
     ##~~ AssetPlugin mixin
     def get_assets(self):
         return {
-            "js": ["js/ARPrintVisualizer.js"],
+            "js": ["js/ARPrintVisualizer.js",
+                   "js/opencv.js"],
             "css": ["css/ARPrintVisualizer.css"],
             "less": ["less/ARPrintVisualizer.less"]
         }
+    
+    ##~~ TemplatePlugin mixin
+    def get_template_configs(self):
+        return [
+            {
+                "type": "webcam",
+                "template": "ARPrintVisualizer_webcam.jinja2",
+                "custom_bindings": True,
+                "suffix": "_real"
+            }
+        ]
+    ##~~ SettingsPlugin mixin
+    def get_settings_version(self):
+        return 1
+    
+    def get_settings_defaults(self):
+        return dict(
+            stream="http://172.30.250.143:8081/video.mjpg",
+            snapshot="http://localhost:8888/out.jpg",
+            flipH=True,
+            cacheBuster=True,
+            snapshotSslValidation=True,
+            snapshotTimeout=5
+        )
+
+    ##~~ WebcamProviderPlugin mixin
+    def get_webcam_configurations(self):
+        stream = self._settings.get(["stream"])
+        snapshot = self._settings.get(["snapshot"])
+        flipH = self._settings.get_boolean(["flipH"])
+        cacheBuster = self._settings.get_boolean(["cacheBuster"])
+        snapshotSslValidation = self._settings.get_boolean(["snapshotSslValidation"])
+
+        try:
+            streamTimeout = int(self._settings.get(["streamTimeout"]))
+        except Exception:
+            streamTimeout = 5
+
+        try:
+            snapshotTimeout = int(self._settings.get(["snapshotTimeout"]))
+        except Exception:
+            snapshotTimeout = 5
+
+        return [
+            Webcam(
+                name="ARCam",
+                displayName="AR Webcam",
+                flipH=flipH,
+                snapshotDisplay=snapshot,
+                canSnapshot=self._can_snapshot(),
+                compat=WebcamCompatibility(
+                    stream=stream,
+                    streamTimeout=streamTimeout,
+                    cacheBuster=cacheBuster,
+                    snapshot=snapshot,
+                    snapshotTimeout=snapshotTimeout,
+                    snapshotSslValidation=snapshotSslValidation,
+                ),
+                extras=dict(
+                    stream=stream,
+                    streamTimeout=streamTimeout,
+                    cacheBuster=cacheBuster,
+                ),
+            ),
+        ]
+
+    def _can_snapshot(self):
+        snapshot = self._settings.get(["snapshot"])
+        return snapshot is not None and snapshot.strip() != ""
+
+    def take_webcam_snapshot(self, _):
+        snapshot_url = self._settings.get(["snapshot"])
+        if not self._can_snapshot():
+            raise WebcamNotAbleToTakeSnapshotException("ARCam")
+
+        with self._capture_mutex:
+            self._logger.debug(f"Capturing image from {snapshot_url}")
+            r = requests.get(
+                snapshot_url,
+                stream=True,
+                timeout=self._settings.get_int(["snapshotTimeout"]),
+                verify=self._settings.get_boolean(["snapshotSslValidation"]),
+            )
+            r.raise_for_status()
+            return r.iter_content(chunk_size=1024)
+
 
     ##~~ Softwareupdate hook
-
     def get_update_information(self):
         # Define the configuration for your plugin to use with the Software Update
         # Plugin here. See https://docs.octoprint.org/en/master/bundledplugins/softwareupdate.html
@@ -48,7 +129,6 @@ class ARPrintVisualizerPlugin(octoprint.plugin.SettingsPlugin,
                 "pip": "https://github.com/jatin-47/OctoPrint-ARPrintVisualizer/archive/{target_version}.zip",
             }
         }
-
 
 __plugin_name__ = "ARPrintVisualizer"
 __plugin_pythoncompat__ = ">=3,<4"  # Only Python 3
--- octoprint_ARPrintVisualizer/static/js/ARPrintVisualizer.js
+++ octoprint_ARPrintVisualizer/static/js/ARPrintVisualizer.js
@@ -7,23 +7,59 @@
 $(function() {
     function ArprintvisualizerViewModel(parameters) {
         var self = this;
+        self.settingsViewModel = parameters[0];
+        self.cam_url=ko.observable();
+        
+        
+        function convertImageToGray(img) {
+            let dst = new cv.Mat();
+            cv.cvtColor(img, dst, cv.COLOR_RGBA2GRAY, 0);
+            return dst;
+        }
 
-        // assign the injected parameters, e.g.:
-        // self.loginStateViewModel = parameters[0];
-        // self.settingsViewModel = parameters[1];
+        self.onBeforeBinding = function() {
+            self.cam_url(self.settingsViewModel.settings.plugins.ARPrintVisualizer.stream());
+            self.flipH = self.settingsViewModel.settings.plugins.ARPrintVisualizer.flipH();
+        }
+        self.onAfterBinding = function() {
+        let imgElement = document.getElementById('ARCam');
+        let src = new cv.Mat(height, width, cv.CV_8UC4);
+        let dst = new cv.Mat(height, width, cv.CV_8UC1);
+        let cap = new cv.VideoCapture(imgElement);
+        const FPS = 30;
+        function processVideo() {
+            try {
+                if (!streaming) {
+                    // clean and stop.
+                    src.delete();
+                    dst.delete();
+                    return;
+                }
+                let begin = Date.now();
+                // start processing.
+                cap.read(src);
+                cv.cvtColor(src, dst, cv.COLOR_RGBA2GRAY);
+                cv.imshow('outcanvas', dst);
+                // schedule the next one.
+                let delay = 1000/FPS - (Date.now() - begin);
+                setTimeout(processVideo, delay);
+            } catch (err) {
+                utils.printError(err);
+            }
+            // schedule the first one.
+        }
+        setTimeout(processVideo, 0);
+        
+        };
 
-        // TODO: Implement your plugin's view model here.
+          
+           
     }
-
-    /* view model class, parameters for constructor, container to bind to
-     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
-     * and a full list of the available options.
-     */
+        
+  
     OCTOPRINT_VIEWMODELS.push({
         construct: ArprintvisualizerViewModel,
-        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
-        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
-        // Elements to bind to, e.g. #settings_plugin_ARPrintVisualizer, #tab_plugin_ARPrintVisualizer, ...
-        elements: [ /* ... */ ]
+        dependencies: [ "settingsViewModel"],
+        elements: [ "#mjpg_container"]
     });
 });
